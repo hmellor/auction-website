94340664849ff7cd9cdc06c0bce69c5aa2a60eac
Harry Mellor <19981378+HMellor@users.noreply.github.com>
2023-04-22 01:34:00 +0100

    Update Firebase version to 9.20.0 on SDK and libraries (#21)
    * Update Firebase version on SDK and libraries
    
    * Fix breaking API changes and update instructions

--- js/popups.js
+++ js/popups.js
@@ -1,12 +1,16 @@
-loginModal = new bootstrap.Modal(document.getElementById('login-modal'))
-infoModal = new bootstrap.Modal(document.getElementById('info-modal'))
-bidModal = new bootstrap.Modal(document.getElementById('bid-modal'))
+// Imports
+import { auth, db, auctions } from "./firebase.js";
+import { doc, setDoc } from "https://www.gstatic.com/firebasejs/9.20.0/firebase-firestore.js";
+
+const loginModal = new bootstrap.Modal(document.getElementById('login-modal'))
+const infoModal = new bootstrap.Modal(document.getElementById('info-modal'))
+export const bidModal = new bootstrap.Modal(document.getElementById('bid-modal'))
 
 function openInfo(id) {
   let i = id.match("[0-9]+");
-  document.getElementById('info-modal-title').innerText = titles[i]
-  document.getElementById('info-modal-desc').innerHTML = details[i]
-  document.getElementById('info-modal-img').src = secondaryImages[i];
+  document.getElementById('info-modal-title').innerText = auctions[i].title
+  document.getElementById('info-modal-desc').innerHTML = auctions[i].detail
+  document.getElementById('info-modal-img').src = auctions[i].secondaryImage;
   document.querySelector("#info-modal > div > div > div.modal-footer > button.btn.btn-primary").id = "info-modal-submit-bid-btn-" + i
   infoModal.show()
 }
@@ -15,9 +19,9 @@ function openBid(id) {
   let i = id.match('[0-9]+');
   document.getElementById("amount-input").value = ""
   document.getElementById("amount-input").classList.remove("is-invalid")
-  document.getElementById('bid-modal-subtitle').innerText = titles[i]
+  document.getElementById('bid-modal-subtitle').innerText = auctions[i].title
   document.querySelector("#bid-modal > div > div > div.modal-footer > button.btn.btn-primary").id = "bid-modal-submit-bid-btn-" + i
-  loggedIn = auth.currentUser && auth.currentUser.displayName
+  let loggedIn = auth.currentUser && auth.currentUser.displayName
   if (loggedIn) {
     bidModal.show()
     document.getElementById("amount-input").focus()
@@ -27,23 +31,23 @@ function openBid(id) {
 }
 
 function openLogin() {
-  loggedIn = auth.currentUser && auth.currentUser.displayName
+  let loggedIn = auth.currentUser && auth.currentUser.displayName
   if (!loggedIn) { loginModal.show(); document.getElementById('username-input').focus() }
 }
 
 function newUserLogin() {
-  loggedIn = auth.currentUser && auth.currentUser.displayName
+  let loggedIn = auth.currentUser && auth.currentUser.displayName
   if (!loggedIn) {
     let username = document.getElementById('username-input').value
     let user = auth.currentUser;
     user.updateProfile({ displayName: username })
-    db.collection("users").doc(user.uid).set({ name: username, admin: false })
+    setDoc(doc(db, "users", user.uid), { name: username, admin: false })
     loginModal.hide()
     replaceSignupButton(username)
   }
 }
 
-function autoLogin() {
+export function autoLogin() {
   auth.onAuthStateChanged(function (user) {
     if (user && user.displayName != null) {
       replaceSignupButton(user.displayName)
@@ -57,3 +61,8 @@ function replaceSignupButton(name) {
   document.getElementById('signup-button').style.display = "none"
   document.getElementById('username-display').innerText = "Hi " + name
 }
+
+window.openLogin = openLogin
+window.newUserLogin = newUserLogin
+window.openBid = openBid
+window.openInfo = openInfo
