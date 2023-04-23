aaafa0c0f2cc3ce4dee28de655a871cf62d53018
HMellor <hej.mellor@gmail.com>
2023-04-23 11:33:03 +0100

    Populate winning column with usernames

--- js/admin.js
+++ js/admin.js
@@ -1,6 +1,6 @@
 // Imports
 import { db } from "./firebase.js";
-import { doc, onSnapshot } from "https://www.gstatic.com/firebasejs/9.20.0/firebase-firestore.js";
+import { doc, getDoc, onSnapshot } from "https://www.gstatic.com/firebasejs/9.20.0/firebase-firestore.js";
 
 let table = document.querySelector("tbody");
 
@@ -24,11 +24,11 @@ function createRow(item) {
 
 export function dataListener() {
     // Listen for updates in active auctions
-    onSnapshot(doc(db, "auction", "items"), (document) => {
+    onSnapshot(doc(db, "auction", "items"), (items) => {
         console.debug("dataListener() read from auction/items")
         // Parse flat document data into structured Object
         let data = {}
-        for (const [key, details] of Object.entries(document.data())) {
+        for (const [key, details] of Object.entries(items.data())) {
             let [item, bid] = key.split("_").map(i => Number(i.match(/\d+/)))
             data[item] = data[item] || {}
             data[item][bid] = details
@@ -45,14 +45,11 @@ export function dataListener() {
             row.children[1].innerText = bids[0].title
             row.children[2].innerText = `£${bids[bidCount].amount.toFixed(2)}`
             row.children[3].innerText = bidCount
+            if (bids[bidCount].uid) {
+                getDoc(doc(db, "users", bids[bidCount].uid)).then((user) => {
+                    row.children[4].innerText = user.get("name")
+                })
+            }
         }
     })
 }
-
-// row.children[3].addEventListener()
-// if (bidCount > 0) {
-//     const docRef = doc(db, "users", bids[bidCount].uid)
-//     getDoc(docRef, user => {
-//         row.children[4].innerText = user.name
-//     })
-// }
