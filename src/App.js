213f7f276ff561f576fd80d995f4c61aceb3db64
HMellor <hej.mellor@gmail.com>
2023-12-17 23:25:04 +0000

    Fix homepage

--- src/App.js
+++ src/App.js
@@ -38,7 +38,7 @@ function App() {
         <Navbar admin={admin} />
         <SignUpModal />
         <Routes>
-          <Route path="/" Component={HomePage} />
+          <Route path="/auction-website" Component={HomePage} />
           <Route
             path="/admin"
             element={
